70cf6925fe1aa17c34b6aca51cea3bc1b99317ec
Timur Bazarin <timurbazarin@gmail.com>
2026-09-25 05:27:08 +0300

    Сделана п1.2.2

--- 121-blink-reg/main.c
+++ 121-blink-reg/main.c
@@ -8,11 +8,16 @@ const uint LED_PIN = 25;
 int main()
 {
    gpio_init(LED_PIN);
-   gpio_set_dir(LED_PIN, GPIO_OUT);
+   //gpio_set_dir(LED_PIN, GPIO_OUT); this is string from lesson
+
+   volatile uint32_t *gpio_set_dir = (uint32_t *)(SIO_BASE + SIO_GPIO_OE_SET_OFFSET);
+   //this is also this string, but realised by register of memory.
 
    volatile uint32_t *gpio_out_set = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_SET_OFFSET);
    volatile uint32_t *gpio_out_clr = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_CLR_OFFSET);
    const uint32_t led_mask = 1u << LED_PIN;
+
+   *gpio_set_dir = led_mask;
 
    while (1)
    {
